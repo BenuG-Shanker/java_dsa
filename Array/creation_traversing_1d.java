cfc51e8502afb400cb3fd6b116842aeecc0f8e60
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2023-08-21 03:11:56 -0700

    array update

--- Array/creation_traversing_1d.java
+++ Array/creation_traversing_1d.java
@@ -1,8 +1,10 @@
+// class is about for creating and traversing the stored date thoroughly in an 1d Array
 package Array;
 import java.util.*;
 class creation_traversing_1d{
     public static void main(String[] args){
         Scanner read = new Scanner(System.in);
+        System.out.print("Size : ");
         int n = read.nextInt();
         int[] a = new int[n];
         for(int i=0; i<n; i++){
--- Array/creation_traversing_2d.java
+++ Array/creation_traversing_2d.java
@@ -1,9 +1,12 @@
+// class is about for creating and traversing the stored date thoroughly in an 2d Array
 package Array;
 import java.util.*;
 class creation_traversing_2d{
     public static void main(String[] args){
         Scanner read = new Scanner(System.in);
+        System.out.print("Row Size : ");
         int n = read.nextInt();
+        System.out.print("Column Size : ");
         int m = read.nextInt();
         int[][] a = new int[n][m];
         for(int i=0; i<n; i++){
