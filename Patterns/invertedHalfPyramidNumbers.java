aec36bd4dc73b886dbefc9a3f99d5fda40169674
BenuG-Shanker <100076428+BenuG-Shanker@users.noreply.github.com>
2025-03-25 14:11:20 +0530

    invertedHalfPyramidNumbersPattern
    A java code for printing inverted half pyramid with numbers pattern

---
+++ Patterns/invertedHalfPyramidNumbers.java
@@ -0,0 +1,20 @@
+// To print the provided pattern
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+public class invertedHalfPyramidNumbers {
+    public static void invertedHalfPyramid(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i+1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        invertedHalfPyramid(5);
+    }
+}
